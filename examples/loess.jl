b59d2dc005b4c1bacf6ff061ed76c87dc56a528b
fredo <frederic.testard@gmail.com>
2023-03-29 23:49:46 +0200

    loess example

---
+++ examples/loess.jl
@@ -0,0 +1,64 @@
+using EasyVega
+
+
+# use the movies database from Vega examples
+# filter out bad data
+movies = Data(
+    url = "https://raw.githubusercontent.com/vega/vega-datasets/next/data/movies.json",
+    transform= [
+        (type= "filter",
+         expr= "datum['Rotten Tomatoes Rating'] != null && datum['IMDB Rating'] != null"
+        )
+    ]
+)
+
+
+# control to change the bandwidth of the loess interpolation
+loessBandwidth = Signal(
+    value= 0.3,
+    bind= (input= "range", min= 0.05, max= 1)
+)
+
+
+# create a loess interpolation
+trend = Data(
+    source= movies,
+    transform= [
+        (
+          type= "loess",
+          # "groupby": [{"signal": "groupby === 'genre' ? 'Major Genre' : 'foo'"}],
+          bandwidth= (signal= loessBandwidth,),
+          x= "Rotten Tomatoes Rating",
+          y= "IMDB Rating",
+          as= ["u", "v"]
+        )
+      ]
+)
+
+
+# define the scale (can't use the movies.xyz shortcut here because the field names have spaces
+#   which prevent using symbols for the getproperty )
+xscale = LinearScale(range="width", domain=(data=movies, field="Rotten Tomatoes Rating"))
+yscale = LinearScale(range="height", domain=(data=movies, field="IMDB Rating"))
+
+
+sma = SymbolMark(
+    from_data= movies,
+    :x => (scale= xscale, field="Rotten Tomatoes Rating"),
+    :y => (scale= yscale, field="IMDB Rating"),
+    :fillOpacity => 0.5,
+    :size => 16
+)
+
+
+lma = LineMark(
+    :x => xscale(trend.u),
+    :y => yscale(trend.v),
+    :stroke => :firebrick
+)
+
+
+VG(width=400, height=400, background=:white, padding= 20,
+    marks=[sma, lma],
+    axes=[xscale(orient="bottom"), yscale(orient="left")]
+)
